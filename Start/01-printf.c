6de4c3d7280a717d71dade4875cc20d343d2bb44
Alberto Noguchi <alberto2611@gmail.com>
2023-09-25 12:30:47 -0300

    chore: add escape sequence ex

--- Start/01-printf.c
+++ Start/01-printf.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-    printf("Hello World");
+    printf("Hello World\n");
+
+    // Escape Sequence
+    printf("Good\nMorning\n");
+
+    printf("Happy\tNew\tYear");
+
+    printf("He says, \"That's good.\"");
+
 
     return 0;
 }
