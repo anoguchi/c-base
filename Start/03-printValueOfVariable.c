3c2b0ec1706da3d64d1f0ff2443b73411210ef21
Alberto Noguchi <alberto2611@gmail.com>
2023-09-25 16:16:35 -0300

    chore: add how to print the value of variable

---
+++ Start/03-printValueOfVariable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int i = 10;
+    char c = 'a';
+
+    printf("Value of i = %d\n", i);
+    printf("Value of c = %c\n", c);
+
+    printf("i = %d \t c = %c \n", i,c);
+
+    return 0;
+}
