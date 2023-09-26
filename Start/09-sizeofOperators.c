68e912334be485bb675ef3d6c1f1a46d365fe816
Alberto Noguchi <alberto2611@gmail.com>
2023-09-26 18:09:57 -0300

    chore: add increment and decrement operators

--- Start/09-sizeofOperators.c
+++ Start/09-sizeofOperators.c
@@ -4,10 +4,20 @@ int main()
 {
     char c;
     double d;
+    int var1 = 5, var2 = 5;
 
     printf("sizeof variable c = %d\n", sizeof(c));
     printf("sizeof variable d = %d\n", sizeof(d));
     printf("sizeof(10) = %d\n", sizeof(10));
+
+    // 5 is displayed
+    // Then, var1 is increased to 6.
+    printf("%d\n", var1++);
+    printf("%d\n", var1);
+
+    // var2 is increased to 6 
+    // Then, it is displayed.
+    printf("%d\n", ++var2);
 
     return 0;
 }
