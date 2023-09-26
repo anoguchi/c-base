464f7b1815167f379a29badb91786551a51d2ed5
Alberto Noguchi <alberto2611@gmail.com>
2023-09-26 17:40:13 -0300

    chore: add sizeof operators

---
+++ Start/09-sizeofOperators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    char c;
+    double d;
+
+    printf("sizeof variable c = %d\n", sizeof(c));
+    printf("sizeof variable d = %d\n", sizeof(d));
+    printf("sizeof(10) = %d\n", sizeof(10));
+
+    return 0;
+}
