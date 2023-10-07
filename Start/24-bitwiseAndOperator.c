b64c000ff752cc95c5646a67979f431afee2c72d
Alberto Noguchi <alberto2611@gmail.com>
2023-10-07 18:27:22 -0300

    chore: add bitwise operators

---
+++ Start/24-bitwiseAndOperator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int a = 5, b = 9;
+  int output = a & b;
+
+  printf("%d", output);
+
+  return 0;
+}
---
+++ Start/28-bitwiseOnesComplementOperator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int variable = 4;
+
+  printf("variable = %d \n", ~variable);
+
+  return 0;
+}
